80221aabfa25617552bb33d48898115f146f9928
Keir Rice <keirrice@gmail.com>
2018-12-12 22:11:21 +1300

    Renamed files to match repo.
    Fixed name clash: Px  -> PORT_x
    Added pinMode method to the PortIDs

--- kPin.cpp
+++ kPin.cpp
@@ -3,24 +3,24 @@
   All the pins.
 *************************************************************/
 #include "Arduino.h"
-#include "kPins.h"
+#include "kPin.h"
 
-namespace kPins{
+namespace kPin{
 
 	void PortID::EnablePCInterupt(uint8_t portMask) const {
 		switch (mPortNumber){
-			case PB:
+			case PORT_B:
 			PCICR |= 0b00000001;
 			PCMSK0 |= portMask;
 			break;
 
-			case PE:
-			case PJ:
+			case PORT_E:
+			case PORT_J:
 			PCICR |= 0b00000010;
 			PCMSK1 |= portMask;
 			break;
 
-			case PK:
+			case PORT_K:
 			PCICR |= 0b00000100;
 			PCMSK2 |= portMask;
 			break;
@@ -31,18 +31,18 @@ namespace kPins{
 	}
 	void PortID::DisablePCInterupt(uint8_t portMask) const {
 		switch (mPortNumber){
-			case PB:
+			case PORT_B:
 			PCMSK0 &= ~portMask;
 			if (PCMSK0 == 0) { PCICR &= ~0b00000001; };
 			break;
 
-			case PE:
-			case PJ:
+			case PORT_E:
+			case PORT_J:
 			PCMSK1 &= ~portMask;
 			if (PCMSK1 == 0) { PCICR &= ~0b00000010; };
 			break;
 
-			case PK:
+			case PORT_K:
 			PCMSK2 &= ~portMask;
 			if (PCMSK2 == 0) { PCICR &= ~0b00000100; };
 			break;
